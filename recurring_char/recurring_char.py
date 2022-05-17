679f76baec2849cce76f5e1269bc4e20d0a03df9
Timz Owen <timzowen@gmail.com>
2022-05-17 20:50:36 +0300

    Update recurring_char.py
    removed django code

--- recurring_char/recurring_char.py
+++ recurring_char/recurring_char.py
@@ -10,31 +10,3 @@ def recurring_char(given_string):
     return None
 
 print(recurring_char('DABCDBCD')) # returns 'D' being the first char and repetitive
-
-
-
-#  Models
-from django.core.exceptions import ValidationError
-from django.db import models
-
-
-class Customer(models.Model):
-    # No need to update these
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    company = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
-    title = models.CharField(max_length=100)
-
-    # TODO: HERE
-    gender = models.CharField(max_length=100)
-    email = models.EmailField()
-    lat = models.IntegerField()
-    lng = models.IntegerField()
-
-    def clean(self):
-        # Make sure both (lat, lng) are set or neither.
-        coords = [self.lat, self.lng]
-        if any(coords) and not all(coords):
-            raise ValidationError("Cannot insert partial coordinates.")
-        super().clean()  # no-op for models.Model inheritance.
